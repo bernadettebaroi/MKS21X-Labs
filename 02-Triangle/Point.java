68e6d87a6842c69676c6869398a1dc4fb8fa23a8
John Doe <bbaroi30@stuy.edu>
2021-10-26 10:35:35 -0400

    changes

---
+++ 02-Triangle/Point.java
@@ -0,0 +1,32 @@
+public class Point{
+  private double x,y;
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+  public Point(Point p){
+    x= p.x;
+    y= p.y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+  public static double distance (Point a, Point b) {
+    return Math.sqrt((b.x-a.x)*(b.x-a.x)+(b.y - a.y)*(b.y - a.y));
+  }
+  public double distanceTo(Point b) {
+    return Math.sqrt((b.x-x)*(b.x-x)+(b.y - y)*(b.y - y));
+  }
+  public String toString(){
+    return "(" + x + ", " + y + ")";
+  }
+  /*Return true when the respective x and y values of the two points differ by at most 0.001% each.
+  *To avoid issues: If one is 0.0, the other must be exactly 0.0.
+  */
+  public boolean equals(Point other){
+
+  }
+}
---
+++ 02-Triangle/Triangle.java
@@ -0,0 +1,23 @@
+public class Triangle {
+
+  /*Return true when the Points of the triangles are all equal() to the corresponding points of the other Triangle.
+  */
+  public boolean equals(Triangle other){
+  }
+
+
+  /*Return "scalene", "isoscelese", or "equilateral"
+  *Make sure to use closeEnough() to determine if two sides are equal because of rounding!
+  */
+  public String classify(){
+  }
+
+
+  /*Return the area of the triangle
+  *Make sure to use the formula: Area = sqrt ( s(s-a)(s-b)(s-c) )
+  *where s= perimeter/2 , and a,b,c are the side lengths.
+  */
+  public double area(){
+  }
+
+}
