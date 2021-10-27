726ad2d021c9a564163429752bc382d92bc49f3d
John Doe <bbaroi30@stuy.edu>
2021-10-27 10:48:31 -0400

    changes

--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -2,26 +2,42 @@ public class Triangle {
   private Point v1, v2, v3;
 
   public double getPerimeter() {
-
+    v1.distanceTo(v2) + v2.distanceTo(v3) + v3.distanceTo(v1);
   }
 
   public Point getVertex(int x) {
     if (x == 1) {
       return v1;
     }
+    if (x == 2) {
+      return v2;
+    }
+    if (x == 3) {
+      return v3;
+    }
   }
 
-  public void setVertex(int,Point) {
-
+  public void setVertex(int x, Point b) {
+    if (x == 1) {
+      v1 = b;
+    }
+    if (x == 2) {
+      v2 = b;
+    }
+    if (x == 3) {
+      v3 = b;
+    }
   }
 
   public String toString() {
-
+    String ans = "{(";
+    ans += v1.getX() + "," + v1.getY() + "), (" + v2.getX() + "," + v2.getY() + "), (" + v3.getX() + "," + v3.getY() + "}"
   }
 
   /*Return true when the Points of the triangles are all equal() to the corresponding points of the other Triangle.
   */
   public boolean equals(Triangle other){
+    if ()
   }
 
 
