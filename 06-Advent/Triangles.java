3bee609662da3a6458d8feea729bdc06a03f95d6
John Doe <bbaroi30@stuy.edu>
2021-11-23 22:09:17 -0500

    changes

--- 06-Advent/Triangles.java
+++ 06-Advent/Triangles.java
@@ -5,39 +5,45 @@ import java.util.ArrayList;
 
 public class Triangles{
   public static void main (String[] args) {
+    String filename = args[0];
+    int num = Integer.parseInt(args[1]);
     try {
-      int ans = 0;
-      File file = new File("data");
-      Scanner input = new Scanner(file);
-      while (input.hasNextLine()) {
-        int x = input.nextInt();
-        int y = input.nextInt();
-        int z = input.nextInt();
-        if (isTriangles(x,y,z)) {
-          ans += 1;
+      if (num == 1) {
+        int ans = 0;
+        File file = new File(filename);
+        Scanner input = new Scanner(file);
+        while (input.hasNextLine()) {
+          int x = input.nextInt();
+          int y = input.nextInt();
+          int z = input.nextInt();
+          if (isTriangles(x,y,z)) {
+            ans += 1;
+          }
         }
-      }
-      System.out.println("Number of valid triangles horizontally: " + ans);
-      
-      int ans1 = 1;
-      File file1 = new File("data");
-      Scanner input1 = new Scanner(file1);
-      ArrayList<Integer>list = new ArrayList<Integer>();
-      while (input1.hasNextLine()) {
-        list.add(input1.nextInt());
-        list.add(input1.nextInt());
-        list.add(input1.nextInt());
-      }
-      for (int i = 0; i < list.size()-6; i++) {
-        int a = list.get(i);
-        int b = list.get(i+3);
-        int c = list.get(i+6);
-        if (isTriangles(a,b,c)) {
-          ans1 += 1;
+        System.out.println("Number of valid triangles horizontally: " + ans);
+        input.close();
+
+      } else if (num == 2) {
+        int ans1 = 1;
+        File file1 = new File(filename);
+        Scanner input1 = new Scanner(file1);
+        ArrayList<Integer>list = new ArrayList<Integer>();
+        while (input1.hasNextLine()) {
+          list.add(input1.nextInt());
+          list.add(input1.nextInt());
+          list.add(input1.nextInt());
         }
+        for (int i = 0; i < list.size()-6; i++) {
+          int a = list.get(i);
+          int b = list.get(i+3);
+          int c = list.get(i+6);
+          if (isTriangles(a,b,c)) {
+            ans1 += 1;
+          }
+        }
+        System.out.println("Number of valid triangles vertically: " + ans1);
+        input1.close();//releases the file from your program
       }
-      System.out.println("Number of valid triangles vertically: " + ans1);
-      input.close();//releases the file from your program
 
     } catch (FileNotFoundException e) {
       System.out.println("Error: File not Found");
