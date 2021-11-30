0d35e1e480e4605ba64861c9103c99ee9e195b93
Bernadette Baroi <bbaroi30@stuy.edu>
2021-11-30 10:59:18 -0500

    changes

--- 07-BinarySearch/BinarySearch.java
+++ 07-BinarySearch/BinarySearch.java
@@ -1,17 +1,37 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class BinarySearch {
   public static void main(String[] args) {
+    int result;
+    result = binarySearch(new int[]{0,0,0,0,0,0,0},0);
+    System.out.println(result);//should be 3
+    result = binarySearch(new int[]{0,0,0,0,0,0,0},1);
+    System.out.println(result);//should be -1
 
+    result = binarySearch(new int[]{0,1,2,3,4,5,6},0);
+    System.out.println(result);//should be 0
+    //...
+    result = binarySearch(new int[]{0,1,2,3,4,5,6},6);
+    System.out.println(result);//should be 6
+    //Tip: You can use a loop to find if it works on 0,1,2,3,4,5,and 6 in this array!
   }
 
-  public static int binarySearch(int[]data,int target){
-    int middle = data.size() / 2 - 1;
-    while (data.get(middle) != target) {
-      if (data.get(middle) < target) {
-        data = d
+  public static int binarySearch(int[]data, int target){
+    int mid = data.length / 2;
+    int start = 0;
+    int end = data.length;
+    while (mid > start && mid < end) {
+      if (data[mid] == target) {
+        return mid;
+      } else if (data[mid] > target) {
+        end = mid;
+        mid = mid / 2;
+      } else if (data[mid] < target){
+        start = mid;
+        mid = (end - start) / 2;
       }
     }
-    return 0;
+    return -1;
   }
 }
---
+++ 07-BinarySearch/Non.java
@@ -0,0 +1,5 @@
+public class Non {
+  public static void main(String[] args) {
+    
+  }
+}
