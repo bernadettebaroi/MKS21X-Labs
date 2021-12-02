fa73a58bbcc533845f034241e11c520c7d8ed6c7
Bernadette Baroi <bbaroi30@stuy.edu>
2021-12-01 23:51:04 -0500

    changes

--- 07-BinarySearch/BinarySearch.java
+++ 07-BinarySearch/BinarySearch.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class BinarySearch {
   public static void main(String[] args) {
@@ -15,11 +16,21 @@ public class BinarySearch {
     result = binarySearch(new int[]{0,1,2,3,4,5,6},6);
     System.out.println(result);//should be 6
     //Tip: You can use a loop to find if it works on 0,1,2,3,4,5,and 6 in this array!
+    Random rd = new Random(); // creating Random object
+    int[] arr = new int[10];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = rd.nextInt(); // storing random integers in an array
+    }
+    Arrays.sort(arr);
+    System.out.println(Arrays.toString(arr));
+    int a = arr[3];
+    System.out.println(a);
+    System.out.println(binarySearch(arr, a));
   }
 
   public static int binarySearch(int[]data, int target){
     int start = 0;
-    int end = data.length -1;
+    int end = data.length - 1;
     while (start <= end) {
       int mid = (start + end) / 2;
       if (data[mid] == target) {
--- 07-BinarySearch/Non.java
+++ 07-BinarySearch/Non.java
@@ -1,5 +1,60 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Non {
   public static void main(String[] args) {
-    
+    String filename = args[0];
+    int num = Integer.parseInt(args[1]);
+    try {
+      if (num == 1) {
+        File file = new File(filename);
+        Scanner input = new Scanner(file);
+        int ans = 0;
+        while (input.hasNextLine()) {
+          String s = input.nextLine();
+          if (isNice(s)) {
+            ans += 1;
+          }
+        }
+        System.out.println(ans);
+        input.close();
+      } else if (num == 2) {
+        File file1 = new File(filename);
+        Scanner input1 = new Scanner(file1);
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("Error: File not Found");
+    }
   }
+
+  public static boolean isNice(String s) {
+    boolean ans = false;
+    String[] charArray = { "a", "e", "i", "o", "u" };
+    String[] restrictions = {"ab", "cd", "pq", "xy"};
+    int vowelcount = 0;
+    for (int i = 0; i < 5;i++) {
+      for (int j = 0; j < s.length();j++) {
+        if (s.substring(j,j+1).equals(charArray[i])) {
+          vowelcount += 1;
+        }
+      }
+    }
+    for (int i = 0; i < s.length() -1 ;i++ ) {
+      if (!(s.substring(i,i+2).equals(restrictions[0])) && !(s.substring(i,i+2).equals(restrictions[1])) && !(s.substring(i,i+2).equals(restrictions[2])) && !(s.substring(i,i+2).equals(restrictions[3]))) {
+        if (vowelcount >= 3) {
+          if (s.substring(i,i+1).equals(s.substring(i+1, i+ 2))) {
+            ans = true;
+          }
+        }
+      } else {
+        ans = false;
+        break;
+      }
+    }
+    return ans;
+  }
+
 }
