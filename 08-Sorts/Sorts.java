30c5bc3cb11d37a2bc7b8832eeade62787cf1fc4
Bernadette Baroi <bbaroi30@stuy.edu>
2021-12-06 01:12:25 -0500

    changes

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -2,29 +2,56 @@ import java.util.Arrays;
 
 public class Sorts{
   public static void main(String[] args) {
-    /*
     int[] test = {5, 1, 0, -5, 12, 16};
     bubbleSort(test);
     System.out.println(Arrays.toString(test));
+
     int[] test1 = {0,0,0,0,0,0};
     bubbleSort(test1);
     System.out.println(Arrays.toString(test1));
+
     int[] test2 = {-1,3,2,1,0,-2};
     bubbleSort(test2);
     System.out.println(Arrays.toString(test2));
-    int[] randoTester = new int[100];
-    int[] randoTester2 = new int[100];
-    for (int i = 0; i < 100;i++){
-      int a = (int)(Math.random()*50 +1);
+
+    int[] test3 = {5, 1, 0, -5, 12, 16};
+    selectionSort(test3);
+    System.out.println(Arrays.toString(test));
+
+    int[] test4 = {0,0,0,0,0,0};
+    selectionSort(test4);
+    System.out.println(Arrays.toString(test1));
+
+    int[] test5 = {-1,3,2,1,0,-2};
+    selectionSort(test5);
+    System.out.println(Arrays.toString(test2));
+
+    int[] randoTester = new int[1000];
+    int[] randoTester2 = new int[1000];
+    for (int i = 0; i < 1000;i++){
+      int a = (int)(Math.random()*100 +1);
       randoTester[i] = a;
       randoTester2[i] = a;
     }
-    Arrays.sort(randoTester2);
-    bubbleSort(randoTester);
-    */
-    int[] test = {5, 1, 0, -5, 12, 16};
-    selectionSort(test);
-    System.out.println(Arrays.toString(test));
+    Arrays.sort(randoTester);
+    bubbleSort(randoTester2);
+    if (equality(randoTester, randoTester2)) {
+      System.out.println("bubbleSort works");
+    }
+
+    int[] randoTester3 = new int[1000];
+    int[] randoTester4 = new int[1000];
+    for (int i = 0; i < 1000;i++){
+      int a = (int)(Math.random()*100 +1);
+      randoTester3[i] = a;
+      randoTester4[i] = a;
+    }
+    Arrays.sort(randoTester3);
+    selectionSort(randoTester4);
+    if (equality(randoTester3, randoTester4)) {
+      System.out.println("selectionSort works");
+    }
+
   }
 
 
@@ -38,6 +65,20 @@ public class Sorts{
           data[j] = a;
         }
       }
+    }
+  }
+
+  public static void selectionSort(int [] ary) {
+    for (int i = 0; i < ary.length -1;i++) {
+      int a = i;
+      for (int j = i + 1; j < ary.length;j++) {
+        if (ary[j] < ary[a]) {
+          a = j;
+        }
+      }
+      int swap = ary[a];
+      ary[a] = ary[i];
+      ary[i] = swap;
     }
   }
 
@@ -55,20 +96,6 @@ public class Sorts{
       }
     }
     return ans;
-  }
-
-  public static void selectionSort(int [] ary) {
-      
-  }
-
-  public static int getMin (int[] ary) {
-    int minValue = ary[0];
-    for(int i=1;i<ary.length;i++){
-      if(ary[i] < minValue){
-        minValue = ary[i];
-      }
-    }
-    return minValue;
   }
 
 }
