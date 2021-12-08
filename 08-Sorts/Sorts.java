2e90e0c2362e1eb8e00432c0176140a7e596739e
Bernadette Baroi <bbaroi30@stuy.edu>
2021-12-07 23:52:10 -0500

    final

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -2,32 +2,33 @@ import java.util.Arrays;
 
 public class Sorts{
   public static void main(String[] args) {
+    /*
     int[] test = {5, 1, 0, -5, 12, 16};
     insertionSort(test);
     System.out.println(Arrays.toString(test));
-/*
+
     int[] test1 = {0,0,0,0,0,0};
-    bubbleSort(test1);
+    insertionSort(test1);
     System.out.println(Arrays.toString(test1));
 
     int[] test2 = {-1,3,2,1,0,-2};
-    bubbleSort(test2);
+    insertionSort(test2);
     System.out.println(Arrays.toString(test2));
 
-    int[] test3 = {5, 1, 0, -5, 12, 16};
-    selectionSort(test3);
-    System.out.println(Arrays.toString(test));
+    int[] test3 = {-4, 2, 0, -5, 222, -16};
+    insertionSort(test3);
+    System.out.println(Arrays.toString(test3));
 
-    int[] test4 = {0,0,0,0,0,0};
-    selectionSort(test4);
-    System.out.println(Arrays.toString(test1));
+    int[] test4 = {-2,-1,0,1,2,3};
+    insertionSort(test4);
+    System.out.println(Arrays.toString(test4));
 
-    int[] test5 = {-1,3,2,1,0,-2};
-    selectionSort(test5);
-    System.out.println(Arrays.toString(test2));
+    int[] test5 = {3,2,1,0,-1,-2};
+    insertionSort(test5);
+    System.out.println(Arrays.toString(test5));
 
-    int[] randoTester = new int[1000];
-    int[] randoTester2 = new int[1000];
+    int[] randoTester = new int[40000];
+    int[] randoTester2 = new int[40000];
     for (int i = 0; i < 1000;i++){
       int a = (int)(Math.random()*100 +1);
       randoTester[i] = a;
@@ -39,8 +40,8 @@ public class Sorts{
       System.out.println("bubbleSort works");
     }
 
-    int[] randoTester3 = new int[1000];
-    int[] randoTester4 = new int[1000];
+    int[] randoTester3 = new int[40000];
+    int[] randoTester4 = new int[40000];
     for (int i = 0; i < 1000;i++){
       int a = (int)(Math.random()*100 +1);
       randoTester3[i] = a;
@@ -50,6 +51,19 @@ public class Sorts{
     selectionSort(randoTester4);
     if (Arrays.equals(randoTester3, randoTester4)) {
       System.out.println("selectionSort works");
+    }
+
+    int[] randoTester5 = new int[40000];
+    int[] randoTester6 = new int[40000];
+    for (int i = 0; i < 1000;i++){
+      int a = (int)(Math.random()*100 +1);
+      randoTester5[i] = a;
+      randoTester6[i] = a;
+    }
+    Arrays.sort(randoTester5);
+    insertionSort(randoTester6);
+    if (Arrays.equals(randoTester5, randoTester6)) {
+      System.out.println("insertionSort works");
     }
 */
   }
@@ -83,16 +97,14 @@ public class Sorts{
   }
 
   public static void insertionSort(int[]data){
-    int a = 0;
     for (int i = 1; i < data.length; i++) {
-      for (int j = i - 1; j >= 0; j--) {
-        if (data[j] <= data[i]) {
-          a = j;
-        }
+      int target = data[i];
+      int b = i - 1;
+      while (b >= 0 && data[b] > target) {
+        data[b+1] = data[b];
+        b -= 1;
       }
-      int swap = data[a];
-      data[a] = data[i];
-      data[i] = swap;
+      data[b+1] = target;
     }
   }
 
