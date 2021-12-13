3d87f41db86af0eaad70538371206158b6b4db94
Bernadette Baroi <bbaroi30@stuy.edu>
2021-12-13 10:45:49 -0500

    changes

--- 09-WordSearch/Driver.java
+++ 09-WordSearch/Driver.java
@@ -2,9 +2,11 @@ public class Driver {
   public static void main(String[] args) {
     WordSearch test = new WordSearch(10,10);
     //System.out.println(test);
+    /*
     test.addWordHorizontal("yes", 2, 2);
     test.addWordVertical("aday", 1, 2);
-    /*
+    test.addWordDiagonal("pepsi",1,2);
+    */
     test.addWord(3, 2, "yes", 0 , 1);
     test.addWord(5, 4, "loves", 0 , -1);
     test.addWord(5, 4, "lake" , 1, 0);
@@ -13,7 +15,6 @@ public class Driver {
     test.addWord(7, 3, "sure", -1 , 0);
     test.addWord(4, 3, "thanks", -1 , 1);
     test.addWord(10, 4, "Pop", -1 , -1);
-    */
     System.out.println(test);
   }
 }
