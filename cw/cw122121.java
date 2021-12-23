95e42353cdc81c23acf0ef502f2046be15d01daf
Bernadette Baroi <bbaroi30@stuy.edu>
2021-12-23 02:08:33 -0500

    finished the methods and tested it

--- cw/cw122121.java
+++ cw/cw122121.java
@@ -3,11 +3,17 @@ public class cw122121 {
       //return the value of Integer.parseInt(s) but do it yourself!
       //Do not use any built in parse methods.
       int ans = 0;
+      int mul = 0;
+      if (s.substring(0,1).equals("-")) {
+        mul = -1;
+      } else {
+        mul = 1;
+      }
       for (int i = 0; i < s.length();i++) {
         char a = s.charAt(i);
-        ans += valueOfDigit(a);
+        ans = ans*10+valueOfDigit(a);
       }
-      return ans;
+      return mul * ans;
   }
   public static int valueOfDigit(char c) {
     //convert '0' to 0, '5' to 5, etc.
@@ -19,7 +25,17 @@ public class cw122121 {
     return ans;
   }
   public static void main(String[] args) {
-    int a = valueOfDigit('0');
+    int a = stringToInt("-145893");
+    System.out.println(a);
+    a = stringToInt("0");
+    System.out.println(a);
+    a = stringToInt("145893");
+    System.out.println(a);
+    a = stringToInt("1");
+    System.out.println(a);
+    a = stringToInt("10");
+    System.out.println(a);
+    a = stringToInt("-100");
     System.out.println(a);
   }
 }
