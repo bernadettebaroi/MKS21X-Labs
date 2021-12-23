35358e2e47b66094382c5bd30b63fb6664dbd854
Bernadette Baroi <bbaroi30@stuy.edu>
2021-12-23 01:36:53 -0500

    added methods

--- cw/cw122121.java
+++ cw/cw122121.java
@@ -1,5 +1,16 @@
 public class cw122121 {
+  public static int stringToInt(String s){
+      //return the value of Integer.parseInt(s) but do it yourself!
+      //Do not use any built in parse methods.
+      return 1;
+  }
+  public static int valueOfDigit(char c) {
+    //convert '0' to 0, '5' to 5, etc.
+    int a = (int)(c);
+    return a;
+  }
   public static void main(String[] args) {
-    
+    int a = valueOfDigit('5');
+    System.out.println(a);
   }
 }
