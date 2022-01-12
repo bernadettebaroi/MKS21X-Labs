a7e13cc87867cec09ae25b44d4905491189a1ccf
Bernadette Baroi <bbaroi30@stuy.edu>
2022-01-12 01:32:59 -0500

    updated code for color border

--- 12-Stuyablo/Terminal.java
+++ 12-Stuyablo/Terminal.java
@@ -1,24 +1,29 @@
 public class Terminal {
   public static void main(String[] args) {
-    int[] intArray = new int[4];
-    Text.hideCursor();
     for (int j = 80; j > 0; j--) {
+      Text.hideCursor();
       Text.go(30,j);
       System.out.println(Text.colorize(" ",Text.BLUE+Text.BACKGROUND));
+      Text.showCursor();
     }
     for (int j = 30; j > 0; j--) {
+      Text.hideCursor();
       Text.go(j,1);
       System.out.println(Text.colorize(" ",Text.BLUE+Text.BACKGROUND));
+      Text.showCursor();
     }
     for (int j = 1; j < 81; j++) {
+      Text.hideCursor();
       Text.go(1,j);
       System.out.println(Text.colorize(" ",Text.BLUE+Text.BACKGROUND));
+      Text.showCursor();
     }
     for (int j = 1; j < 31; j++) {
+      Text.hideCursor();
       Text.go(j,80);
       System.out.println(Text.colorize(" ",Text.BLUE+Text.BACKGROUND));
+      Text.showCursor();
     }
     Text.reset();
-    Text.showCursor();
   }
 }
