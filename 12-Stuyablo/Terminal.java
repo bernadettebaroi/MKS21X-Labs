7f68b6957488c435ef6ec6fbe4fbb182c43c0849
Bernadette Baroi <bbaroi30@stuy.edu>
2022-01-12 01:17:54 -0500

    added coding for the border

--- 12-Stuyablo/Terminal.java
+++ 12-Stuyablo/Terminal.java
@@ -2,9 +2,21 @@ public class Terminal {
   public static void main(String[] args) {
     int[] intArray = new int[4];
     Text.hideCursor();
+    for (int j = 80; j > 0; j--) {
+      Text.go(30,j);
+      System.out.println(Text.colorize(" ",Text.BLUE+Text.BACKGROUND));
+    }
+    for (int j = 30; j > 0; j--) {
+      Text.go(j,1);
+      System.out.println(Text.colorize(" ",Text.BLUE+Text.BACKGROUND));
+    }
     for (int j = 1; j < 81; j++) {
       Text.go(1,j);
-      System.out.println(colorize(" ",Text.BLUE+Text.BACKGROUND));
+      System.out.println(Text.colorize(" ",Text.BLUE+Text.BACKGROUND));
+    }
+    for (int j = 1; j < 31; j++) {
+      Text.go(j,80);
+      System.out.println(Text.colorize(" ",Text.BLUE+Text.BACKGROUND));
     }
     Text.reset();
     Text.showCursor();
