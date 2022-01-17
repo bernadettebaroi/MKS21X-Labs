f2c48e778c3f5149d630054c099673b8bf2f7882
Bernadette Baroi <bbaroi30@stuy.edu>
2022-01-17 00:18:06 -0500

    almost done with the code just need to fix the color thingy

--- 12-Stuyablo/StuyabloGame.java
+++ 12-Stuyablo/StuyabloGame.java
@@ -12,29 +12,29 @@ public class StuyabloGame{
   //Display a List of 1-4 adventurers on the rows row through row+3 (4 rows max)
   //Should include Name and HP on 2 separate lines. (more to be added later)
   public static void drawParty(ArrayList<Adventurer> party,int startRow){
-    int k = 2;
-    for (int i = 0; i < party.size(); i ++) {
-      Text.go(2,k);
-      System.out.print(Text.colorize("" + party.get(i),Text.WHITE));
-      k += 25;
-    }
     int l = 2;
     for (int i = 0; i < party.size(); i ++) {
-      Text.go(3,l);
-      int b = party.get(i).getSpecialMax() * (1/4);
+      Text.go(startRow,l);
+      System.out.print(Text.colorize("" + party.get(i),Text.MAGENTA));
+
+      Text.go(startRow+1,l);
       String word = "" + party.get(i).getSpecialName() + ": " + party.get(i).getSpecial() + "/" + party.get(i).getSpecialMax();
-      if (party.get(i).getSpecial() <= b) {
+      if (party.get(i).getSpecial() / party.get(i).getSpecialMax() * 100 < 25) {
         System.out.print(Text.colorize(word,Text.RED));
-      } else if (party.get(i).getSpecial() >= b) {
+      } else if (party.get(i).getSpecial() / party.get(i).getSpecialMax() * 100 > 75) {
         System.out.print(Text.colorize(word,Text.GREEN));
+      } else {
+        System.out.print(Text.colorize(word,Text.YELLOW));
       }
-      Text.go(4,l);
-      int a = party.get(i).getmaxHP() * (1/4);
+
+      Text.go(startRow+2,l);
       word = "HP: " + party.get(i).getHP() + "/" + party.get(i).getmaxHP();
-      if (party.get(i).getHP() <= a) {
+      if (party.get(i).getHP() / party.get(i).getmaxHP() * 100 < 25) {
         System.out.print(Text.colorize(word,Text.RED));
-      } else if (party.get(i).getHP() >= a) {
+      } else if (party.get(i).getHP() / party.get(i).getmaxHP() * 100 > 75) {
         System.out.print(Text.colorize(word,Text.GREEN));
+      } else {
+        System.out.print(Text.colorize(word,Text.YELLOW));
       }
       l += 25;
     }
@@ -107,11 +107,17 @@ public class StuyabloGame{
       //display event based on last turn's input
       if(partyTurn){
         //Process user input:
-        if(input.equals("attack")){
+        if(input.equals("attack") || input.equals("")){
+          int change = enemies.get(0).getHP();
           party.get(whichPlayer).attack(enemies.get(0));
+          change = change - enemies.get(0).getHP();
+          drawText(""+ party.get(whichPlayer) + " attacked " + enemies.get(0) + " for " + change + " damage.",HEIGHT/2 -1);
         }
         else if(input.equals("special")){
+          int change = enemies.get(0).getHP();
           party.get(whichPlayer).specialAttack(enemies.get(0));
+          change = change - enemies.get(0).getHP();
+          drawText(""+ party.get(whichPlayer) + " attacked " + enemies.get(0) + " for " + change + " damage.",HEIGHT/2 -1);
         }
         whichPlayer++;
 
@@ -128,8 +134,11 @@ public class StuyabloGame{
         //this block ignores user input!
         //display enemy attack except on turn 0.
         if(turn > 0){
-          int x = (int)(Math.random()*2)+1 ;
-          enemies.get(1).attack(party.get(x));
+          int x = (int)(Math.random()*3);
+          int change = party.get(x).getHP();
+          enemies.get(0).attack(party.get(x));
+          change = change - party.get(x).getHP();
+          drawText(""+ enemies.get(0)+ " attacked " + party.get(x) + " for " + change + " damage.",HEIGHT/2 -1);
 
           //Enemy action choices go here!
         }
